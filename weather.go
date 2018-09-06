77439b16cf9665791bb9d6eb2ac87d274ae01cc2
Tom Camp <tomcamp@comcast.net>
2018-09-05 20:11:37 -0600

    WPi-01: Updating URL method.

--- weather.go
+++ weather.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	response, err := http.Get("https://api.openweathermap.org/data/2.5/weather?zip=81428,us&appid=a5708c2191b6429113e3e22e2dcd3a63&units=imperial")
+	baseURL, err := url.Parse("https://api.openweathermap.org/data/2.5/weather")
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
+		log.Fatal(err)
+	}
+	params := url.Values{}
+	params.Add("zip", "81428,us")
+	params.Add("appid", "a5708c2191b6429113e3e22e2dcd3a63")
+	params.Add("units", "imperial")
+	baseURL.RawQuery = params.Encode()
+	response, err := http.Get(baseURL.String())
+	if err != nil {
+		log.Fatal(err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Printf(string(data))
